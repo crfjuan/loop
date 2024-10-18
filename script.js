ec43528eef8624bb3fee2efdd415fce52134ee18
JUAN DAVID <davidjjuan09@gmail.com>
2024-10-18 15:41:12 -0300

    loop

---
+++ script.js
@@ -0,0 +1,24 @@
+// Loops
+
+let numero = 1;
+// number
+// os números são dados do tipo de number
+
+console.log(numero);
+console.log(numero + 1)
+
+// for
+// estrutura do for
+// for(variável inicial; condião; iteração){}
+// ++ = +1
+// -- = -1
+for(let i = 0;i <= 5; i++){
+    console.log(i)
+}
+
+// while
+let contador = 0;
+    while(contador < 12){
+        contador++;
+        console.log(contador)
+    }
